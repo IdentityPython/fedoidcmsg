dbb7210b0449602ba878e5b3baa392b6b64e35d9
roland <roland@catalogix.se>
2017-11-25 19:30:57 +0100

    Dependent on oicmsg not oiccli

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Topic :: Software Development :: Libraries :: Python Modules"],
     install_requires=[
-         'oiccli',
+         'oicmsg',
          "Cherrypy",
          'cherrypy-cors >= 1.5'],
     zip_safe=False,
